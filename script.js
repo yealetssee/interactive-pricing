346f108c589dfccd253c0261d75b6a13631a1e5c
yealetssee <gegilabadze@gmail.com>
2023-04-07 20:50:22 +0400

    updated javascript functionality

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let toggle = document.querySelector(".toggle");
+let price = document.querySelector(".changing-price");
+let views = document.querySelector(".views");
 
 let range_input = document.querySelector(`input[type="range"]`);
 
@@ -17,4 +19,21 @@ range_input.addEventListener("input", (e) => {
   let target = e.target;
 
   target.style.backgroundSize = `${val}% 100%`;
+
+  if (val <= 20) {
+    views.innerText = "10K pageviews";
+    price.innerText = "$8 ";
+  } else if (val > 20 && val <= 40) {
+    views.innerText = "50K pageviews";
+    price.innerText = "$12 ";
+  } else if (val > 40 && val <= 60) {
+    views.innerText = "100K pageviews";
+    price.innerText = "$16 ";
+  } else if (val > 60 && val <= 80) {
+    views.innerText = "500K pageviews";
+    price.innerText = "$24 ";
+  } else if (val > 80 && val <= 100) {
+    views.innerText = "1M pageviews";
+    price.innerText = "$36 ";
+  }
 });
