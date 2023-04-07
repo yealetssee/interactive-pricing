3ea52cfacfe0d01a8672db39715b9d57edbc2d68
yealetssee <gegilabadze@gmail.com>
2023-04-07 16:07:20 +0400

    added responsive design

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ let range_input = document.querySelector(`input[type="range"]`);
 let clicked = false;
 toggle.addEventListener("click", () => {
   clicked = !clicked;
-  if (clicked == true) {
+  if (clicked) {
     toggle.style.backgroundColor = `${"#7AEADF"}`;
   } else {
     toggle.style.backgroundColor = `${"#CFD8EF"}`;
   }
-  console.log(clicked);
 });
 
 range_input.addEventListener("input", (e) => {
